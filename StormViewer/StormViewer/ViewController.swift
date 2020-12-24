e74a8f8ef73a3ce8589212a04f550307ef8c41dc
jiho-s <40516042+jiho-s@users.noreply.github.com>
2020-12-24 15:45:53 +0900

    Initial StormViewer

---
+++ StormViewer/StormViewer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StormViewer
+//
+//  Created by 신지호 on 2020/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

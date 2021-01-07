d3b13f8459fbbe9b97f81f9eb48e69c6d7d1966b
jiho-s <40516042+jiho-s@users.noreply.github.com>
2021-01-07 15:43:44 +0900

    initial WordScramble

---
+++ WordScramble/WordScramble/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WordScramble
+//
+//  Created by 신지호 on 2021/01/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
